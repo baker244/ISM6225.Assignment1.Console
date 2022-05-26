4f3befdb3dc4ff5df1342f7685d0e6433f4987e1
Caleb Baker <kaylub98@hotmail.com>
2022-05-26 14:54:17 -0400

    Updated the method to return inital and final index only.

--- ISM6225.Assignment1.Console/Program.cs
+++ ISM6225.Assignment1.Console/Program.cs
@@ -6,8 +6,9 @@ namespace ISM6225.Assignment1.Console
     {
         static void Main(string[] args)
         {
-            var marks = new int[] { 5, 6, 6, 9, 9, 12 };
-            var target = 9;
+            #region Call Question 1
+            var marks = new int[] { 5, 9, 6, 9, 9, 12 };
+            var target = 5;
 
             var result = TargetRange(marks, target);
 
@@ -15,6 +16,7 @@ namespace ISM6225.Assignment1.Console
                 System.Console.WriteLine("Target does not exist: [-1, -1]");
             else
                 System.Console.WriteLine("Target exists: [" + string.Join(", ", result) + "]");
+            #endregion
         }
 
         /// <summary>
@@ -33,11 +35,18 @@ namespace ISM6225.Assignment1.Console
                     result.Add(i);
             }
 
+            // If the target wasn't found, include -1, -1
             if (result.Count == 0)
             {
                 result.Add(-1);
                 result.Add(-1);
             }
+            // If the target was only found once, include the same index again as the final index
+            else if (result.Count == 1)
+                result.Add(result[0]);
+            // If the target was found more than two times, use RemoveRange to include the initial and final only
+            else if (result.Count > 2)
+                result.RemoveRange(1, result.Count - 2);
 
             return result.ToArray();
         }
